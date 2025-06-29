b632b44fe974afd0b3cf1eb479010678ef8d81ae
Tarek Salame <tareq.salame@gmail.com>
2025-06-29 19:37:13 +0300

    last

--- src/Components/ModelViewer.jsx
+++ src/Components/ModelViewer.jsx
@@ -2,15 +2,16 @@ import React, { Suspense } from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls, Environment, useGLTF } from "@react-three/drei";
 import "./Hero.css";
+import buildingUrl from "./../models/building.glb";
 
 function BuildingModel() {
-  const model = useGLTF("/models/building.glb"); // ضع الملف داخل public/models
+  const model = useGLTF(buildingUrl);
   return <primitive object={model.scene} scale={1} />;
 }
 
 export default function ModelViewer() {
   return (
-    <div className="ModelViewerDiv" >
+    <div className="ModelViewerDiv">
       <Canvas camera={{ position: [5, 1.5, 4], fov: 30 }}>
         <ambientLight intensity={0.6} />
         <directionalLight position={[10, 10, 5]} intensity={1} />
